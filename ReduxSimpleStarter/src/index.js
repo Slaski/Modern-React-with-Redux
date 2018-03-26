5a9a175cd91904956f4b0c6a45b83a0e24c6894b
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-25 23:11:14 -0300

    feat: Added a SearchBar component.

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
 
 const API_KEY = 'AIzaSyBBqg2ToTswsyRBwI8KuL0bdGDSV9M_eJQ';
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
