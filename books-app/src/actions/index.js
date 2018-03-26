a9183ee4ff47eaf13ad52a0cbaec390da45af9d6
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-26 15:51:41 -0300

    feat: Finished implementing the BOOK_SELECT action to work in the BookDetail and BookList containers.

--- books-app/src/actions/index.js
+++ books-app/src/actions/index.js
@@ -2,6 +2,6 @@ export function selectBook(book) {
     // selectBook is an ActionCreator, it needs to return an action, an object with the type property
     return  {
         type: 'BOOK_SELECTED',
-        book
+        payload: book
     };
 }
