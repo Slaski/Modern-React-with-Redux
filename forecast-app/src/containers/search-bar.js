45a45c4d4ab2622e7a3f88658ad6e705953fe104
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-26 22:44:32 -0300

    feat: Implemented the FETCH_WEATHER action and implemented it on the SearchBar container.

--- forecast-app/src/containers/search-bar.js
+++ forecast-app/src/containers/search-bar.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions';
 
 class SearchBar extends Component {
     constructor(props) {
@@ -20,6 +23,8 @@ class SearchBar extends Component {
         event.preventDefault();
 
         // We need to go and fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
     }
 
     render() {
@@ -39,4 +44,8 @@ class SearchBar extends Component {
     }
 }
 
-export default SearchBar;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
